6d3c80d3c749bc2c96755b319472ab3b542ff12c
Konan Sasaki <konan.5820k@gmail.com>
2023-06-18 14:06:34 +0900

    changed server.py

--- testCode/Socket/Success/server.py
+++ testCode/Socket/Success/server.py
@@ -1,28 +1,29 @@
 import socket
 
-def server(host,port):
-    serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-    serversock.bind((host,port)) #IPとPORTを指定してバインドします
-    serversock.listen(1) #接続の待ち受けをします（キューの最大数を指定）
-    OK = "OK"
-    print('Waiting for connections...')
-    clientsock, client_address = serversock.accept() #接続されればデータを格納
-    print("success connect")
-    while True:
+class TCP():
+    def __init__(self,host,port):
+        serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        serversock.bind((host,port)) #IPとPORTを指定してバインドします
+        serversock.listen(1) #接続の待ち受けをします（キューの最大数を指定）
+        self.OK = "OK"
+        print('Waiting for connections...')
+        self.clientsock, self.client_address = serversock.accept() #接続されればデータを格納
+        print("success connect")
+
+    def communicate(self):
         try:
-            rcvmsg = clientsock.recv(512).decode()
-            if(rcvmsg == ""):
-                continue
+            rcvmsg = self.clientsock.recv(512).decode()
             print(f'Received : {rcvmsg}')
-            print(f'Send : {OK}')
-            clientsock.sendall(OK.encode())
+            print(f'Send : {self.OK}')
+            self.clientsock.sendall(self.OK.encode())
         except BrokenPipeError:
            pass
-    clientsock.close()
 
 if __name__ == "__main__":
-    host = "127.0.0.1"
+    host = "172.18.176.1"
     port = 6969
-    server(host,port)
-
+    comm = TCP(host,port)
+    comm.communicate()
+    comm.communicate()
+    comm.clientsock.close()
